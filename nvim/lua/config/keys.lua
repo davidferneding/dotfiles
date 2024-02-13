2c4acc2db920e76665b96f5887249ac699297a06
David Ferneding <fernedingd@gmail.com>
2024-02-13 18:18:05 +0100

    nvim: add treesitter

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -5,5 +5,9 @@ vim.g.maplocalleader = ","
 -- REMAPPINGS
 local map = vim.api.nvim_set_keymap
 
-map('i', '<C-e>x', '<esc><cmd>:Rex<cr>', {}) -- return to file explorer on ctrl-e -> x
+-- return to file explorer on ctrl-e -> x
+map('i', '<C-e>x', '<esc><cmd>:Rex<cr>', {})
+map('n', '<C-e>x', '<cmd>:Rex<cr>', {})
+
+
 map('i', '<C-r>lua', '<cmd>:w<cr><cmd>:luafile %<cr>', {}) -- save and reload lua file
