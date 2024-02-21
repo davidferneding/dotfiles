9af2d63f03a0494c6c08efd6e8d63d9106d34c5b
David Ferneding <fernedingd@gmail.com>
2024-02-21 14:38:30 +0100

    nvim: add motion keybinds

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -9,10 +9,16 @@ function cmd (command)
     return '<cmd>' .. command .. '<cr>'
 end
 
+-- saving / reloading
+bind({'i', 'n'}, '<C-s>f', cmd(':w')) -- save file
+bind({'i', 'n'}, '<C-s>a', cmd(':wa')) -- save all
+bind({'i', 'n'}, '<C-s>q', cmd(':xa')) -- save all and quit
+bind({'i', 'n'}, '<C-r>f', cmd(':w') .. cmd(':luafile %')) -- save and reload lua file
 
 -- return to file explorer on ctrl-e -> x
 bind('i', '<C-e>x', '<esc>'.. cmd(':Rex'))
 bind('n', '<C-e>x', cmd(':Rex'))
 
--- save and reload lua file
-bind({'i', 'n'}, '<C-r>f', cmd(':w') .. cmd(':luafile %'))
+-- new lines without leaving normal mode
+bind('n', '<leader>o', 'o<Esc>') -- below
+bind('n', '<leader>O', 'O<Esc>') -- above
