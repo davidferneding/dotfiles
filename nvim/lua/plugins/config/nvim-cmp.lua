8dca7ce3e241657ce7253e3d09c52851b165324f
David Ferneding <fernedingd@gmail.com>
2024-02-22 15:24:58 +0100

    nvim: fix tab completion for nvim-cmp

--- nvim/lua/plugins/config/nvim-cmp.lua
+++ nvim/lua/plugins/config/nvim-cmp.lua
@@ -25,6 +25,7 @@ cmp.setup({
                     local entry = cmp.get_selected_entry()
                     if not entry then
                         cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        cmp.confirm()
                     else
                         cmp.confirm()
                     end
