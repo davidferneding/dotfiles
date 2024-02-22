e616804f23b553c35b84b586eeae1856cb0e2595
David Ferneding <fernedingd@gmail.com>
2024-02-22 15:35:04 +0100

    nvim: fix cmp/vsnip config

--- nvim/lua/plugins/config/nvim-cmp.lua
+++ nvim/lua/plugins/config/nvim-cmp.lua
@@ -35,13 +35,9 @@ cmp.setup({
             end, {"i","s","c",}),
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'buffer' },
+        { name = 'nvim_lsp', keyword_length = 1 },
+        { name = 'vsnip', keyword_length = 2 },
+        { name = 'buffer', keyword_length = 3 }
     })
 })
 
@@ -74,8 +70,3 @@ cmp.setup.cmdline(':', {
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---     capabilities = capabilities
--- }
