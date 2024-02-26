a3dc1020817699ffa8ceca25430a726e409da9b0
David Ferneding <fernedingd@gmail.com>
2024-02-26 12:57:52 +0100

    nvim: open file on creation

--- nvim/lua/plugins/config/nvim-tree.lua
+++ nvim/lua/plugins/config/nvim-tree.lua
@@ -38,3 +38,8 @@ require("nvim-tree").setup({
 		table.sort(nodes, natural_cmp)
 	end,
 })
+
+local api = require("nvim-tree.api")
+api.events.subscribe(api.events.Event.FileCreated, function(file)
+  vim.cmd("edit " .. file.fname)
+end)
