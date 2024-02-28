387757661383c47a76f776352d1bc7e970d779b1
David Ferneding <fernedingd@gmail.com>
2024-02-28 19:22:41 +0100

    nvim: remove nvim-tree auto open file on add

--- nvim/lua/plugins/config/nvim-tree.lua
+++ nvim/lua/plugins/config/nvim-tree.lua
@@ -46,8 +46,3 @@ require("nvim-tree").setup({
         }
     },
 })
-
-local api = require("nvim-tree.api")
-api.events.subscribe(api.events.Event.FileCreated, function(file)
-  vim.cmd("edit " .. file.fname)
-end)
