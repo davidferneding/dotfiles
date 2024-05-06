8b4b92bd5efdbdb857848a917b3e6cd169fc08f1
David Ferneding <fernedingd@gmail.com>
2024-05-06 19:53:46 +0200

    nvim: move config init to fnl

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,2 +1,3 @@
-require 'config.keys'
-require 'config.vars'
+-- [nfnl] Compiled from fnl/config/init.fnl by https://github.com/Olical/nfnl, do not edit.
+require("config.keys")
+return require("config.vars")
