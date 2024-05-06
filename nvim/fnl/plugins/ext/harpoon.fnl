86b8db59cd7ddd648385639052b33f77a2ad6fad
David Ferneding <fernedingd@gmail.com>
2024-05-06 21:01:34 +0200

    nvim: move harpoon config to fnl

---
+++ nvim/fnl/plugins/ext/harpoon.fnl
@@ -0,0 +1,40 @@
+{1 :ThePrimeagen/harpoon
+ :branch :harpoon2
+ :dependencies [:nvim-lua/plenary.nvim]
+ :config (fn []
+           (local harpoon (require :harpoon))
+           ;; REQUIRED
+           (harpoon:setup)
+           ;; REQUIRED
+           ;;
+           ;; define functions to configure keybinds in keys.fnl
+           (vim.api.nvim_create_user_command :HarpoonAppend
+                                             (fn []
+                                               (local list (harpoon:list))
+                                               (list:add))
+                                             {})
+           (vim.api.nvim_create_user_command :HarpoonRemove
+                                             (fn []
+                                               (local list (harpoon:list))
+                                               (list:remove))
+                                             {})
+           (vim.api.nvim_create_user_command :HarpoonShow
+                                             (fn []
+                                               (harpoon.ui:toggle_quick_menu (harpoon:list)))
+                                             {})
+           (vim.api.nvim_create_user_command :HarpoonSwitch
+                                             (fn [opts]
+                                               (local list (harpoon:list))
+                                               (list:select (tonumber opts.args)))
+                                             {:nargs 1})
+           (vim.api.nvim_create_user_command :HarpoonNext
+                                             (fn []
+                                               (local list (harpoon:list))
+                                               (list:next))
+                                             {})
+           (vim.api.nvim_create_user_command :HarpoonPrev
+                                             (fn []
+                                               (local list (harpoon:list))
+                                               (list:prev))
+                                             {}))}
+
--- nvim/lua/plugins/ext/harpoon.lua
+++ nvim/lua/plugins/ext/harpoon.lua
@@ -1,37 +1,35 @@
-return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local harpoon = require("harpoon")
-
-        -- REQUIRED
-        harpoon:setup()
-        -- REQUIRED
-
-        -- define functions to configure keybinds in keys.lua
-        vim.api.nvim_create_user_command('HarpoonAppend', function()
-            harpoon:list():add()
-        end, {})
-
-        vim.api.nvim_create_user_command('HarpoonRemove', function()
-            harpoon:list():remove()
-        end, {})
-
-        vim.api.nvim_create_user_command('HarpoonShow', function()
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-        end, {})
-
-        vim.api.nvim_create_user_command('HarpoonSwitch', function(opts)
-            harpoon:list():select(tonumber(opts.args))
-        end, { nargs = 1 })
-
-        vim.api.nvim_create_user_command('HarpoonNext', function()
-            harpoon:list():next()
-        end, {})
-
-        vim.api.nvim_create_user_command('HarpoonPrev', function()
-            harpoon:list():prev()
-        end, {})
-    end
-}
+-- [nfnl] Compiled from fnl/plugins/ext/harpoon.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local harpoon = require("harpoon")
+  harpoon:setup()
+  local function _2_()
+    local list = harpoon:list()
+    return list:add()
+  end
+  vim.api.nvim_create_user_command("HarpoonAppend", _2_, {})
+  local function _3_()
+    local list = harpoon:list()
+    return list:remove()
+  end
+  vim.api.nvim_create_user_command("HarpoonRemove", _3_, {})
+  local function _4_()
+    return (harpoon.ui):toggle_quick_menu(harpoon:list())
+  end
+  vim.api.nvim_create_user_command("HarpoonShow", _4_, {})
+  local function _5_(opts)
+    local list = harpoon:list()
+    return list:select(tonumber(opts.args))
+  end
+  vim.api.nvim_create_user_command("HarpoonSwitch", _5_, {nargs = 1})
+  local function _6_()
+    local list = harpoon:list()
+    return list:next()
+  end
+  vim.api.nvim_create_user_command("HarpoonNext", _6_, {})
+  local function _7_()
+    local list = harpoon:list()
+    return list:prev()
+  end
+  return vim.api.nvim_create_user_command("HarpoonPrev", _7_, {})
+end
+return {"ThePrimeagen/harpoon", branch = "harpoon2", dependencies = {"nvim-lua/plenary.nvim"}, config = _1_}
