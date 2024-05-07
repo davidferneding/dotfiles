e8af9c9591bdd46680ce064b6df716931c08c44d
David Ferneding <fernedingd@gmail.com>
2024-05-07 10:56:26 +0200

    nvim: remove rainbow-delimiters (its painfully slow)

--- nvim/fnl/plugins/ext/rainbow-delimiters.fnl
+++
@@ -1,2 +0,0 @@
-{1 :hiphish/rainbow-delimiters.nvim :version "*"}
-
--- nvim/lua/plugins/ext/rainbow-delimiters.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/ext/rainbow-delimiters.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"hiphish/rainbow-delimiters.nvim", version = "*"}
