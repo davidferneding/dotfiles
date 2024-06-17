bae415dbe26cc0352c836cfdd96df5e7e86bb695
David Ferneding <fernedingd@gmail.com>
2024-06-17 16:56:01 +0200

    nvim: move mason-lock config to fnl

--- nvim/lua/plugins/ext/mason-lock.lua
+++ nvim/lua/plugins/ext/mason-lock.lua
@@ -1,8 +1,2 @@
-return {
-    "zapling/mason-lock.nvim",
-    init = function()
-        require("mason-lock").setup({
-            lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json" -- (default)
-        })
-    end
-}
+-- [nfnl] Compiled from fnl/plugins/ext/mason-lock.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"zapling/mason-lock.nvim", opts = {lockfile_path = (vim.fn.stdpath("config") .. "/mason-lock.json")}}
