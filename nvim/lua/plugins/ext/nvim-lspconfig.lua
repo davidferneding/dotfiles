166fd0ffd7c2fbfc31e66dbb24a29880c8e0b354
David Ferneding <fernedingd@gmail.com>
2024-09-02 15:07:42 +0200

    nvim: migrate nvim-lspconfig.nvim config to fnl

--- nvim/lua/plugins/ext/nvim-lspconfig.lua
+++ nvim/lua/plugins/ext/nvim-lspconfig.lua
@@ -1,4 +1,2 @@
-return {
-    'neovim/nvim-lspconfig',
-    version = "*"
-}
+-- [nfnl] Compiled from fnl/plugins/ext/nvim-lspconfig.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"neovim/nvim-lspconfig", version = "*"}
