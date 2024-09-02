54b22b5299cfa20531ba99304f7c3ee10300dd50
David Ferneding <fernedingd@gmail.com>
2024-09-02 13:37:39 +0200

    nvim: migrate noice.nvim config to fnl

--- nvim/lua/plugins/ext/noice.lua
+++ nvim/lua/plugins/ext/noice.lua
@@ -1,40 +1,8 @@
-return {
-    "folke/noice.nvim",
-    version = "*",
-    event = "VeryLazy",
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-    },
-    config = function()
-        require("noice").setup({
-            cmdline = {
-                view = "cmdline"
-            },
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,   -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = true, -- add a border to hover docs and signature help
-            },
-        })
-
-        require("notify").setup({
-            background_colour = "#000000",
-        })
-    end
-}
+-- [nfnl] Compiled from fnl/plugins/ext/noice.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local noice = require("noice")
+  noice.setup({cmdline = {view = "cmdline"}, lsp = {override = {[{"vim.lsp.util.convert_input_to_markdown_lines"}] = true, [{"vim.lsp.util.stylize_markdown"}] = true, [{"cmp.entry.get_documentation"}] = true}}, presets = {bottom_search = true, command_palette = true, long_message_to_split = true, lsp_doc_border = true, inc_rename = false}})
+  local notify = require("notify")
+  return notify.setup({background_colour = "#000000"})
+end
+return {"folke/noice.nvim", version = "*", event = "VeryLazy", dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}, config = _1_}
