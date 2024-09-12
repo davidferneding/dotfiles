bd513b4464cacc54e4c794c1bea0c7a90b270701
David Ferneding <fernedingd@gmail.com>
2024-09-12 19:13:56 +0200

    feat: add install script for mac

---
+++ install_mac.sh
@@ -0,0 +1,19 @@
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Tools/Shell/Misc
+brew install tmux
+brew install borkdude/brew/babashka
+
+# Apps
+brew install --cask firefox@developer-edition
+
+brew install --cask wezterm
+
+brew install neovim
+
+brew install --cask nikitabobko/tap/aerospace
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install borders
