6c158b5890aa7f47573c7b37777bd610c42ff5ce
David Ferneding <fernedingd@gmail.com>
2024-09-18 18:20:05 +0200

    sketchybar: translate space plugin to clj, remove unused bash scripts

--- sketchybar/plugins/space.clj
+++ sketchybar/plugins/space.clj
@@ -4,13 +4,15 @@
 (require '[babashka.process :refer [shell]]
          '[clojure.string :as string]
          '[sketchybar.core :as sketchybar]
-         '[config]
          '[clojure.edn :as edn])
 
 (defn extract-key [text]
   (string/replace text #" \|.*" ""))
 
 (defn get-monitors [] (map extract-key (string/split (:out (shell {:out :string :err :string :continue true} "aerospace list-monitors")) #"\n")))
+
+(defn get-focused-workspace [monitor]
+  (first (string/split (:out (shell {:out :string :err :string :continue true} "aerospace list-workspaces --monitor" monitor "--visible")) #"\n")))
 
 (defn get-workspaces [monitor]
   (string/split (:out (shell {:out :string :err :string :continue true} "aerospace list-workspaces --monitor" monitor)) #"\n"))
@@ -26,19 +28,6 @@
 (defn space-key [space]
   (str "space." space))
 
-(defn add-space [space monitor]
-  (sketchybar/exec
-   (sketchybar/add-space (space-key space) :left)
-   (sketchybar/set (space-key space) (conj config/item {:space space
-                                                        :icon space
-                                                        :icon.highlight_color (:red config/colors)
-                                                        :display monitor
-                                                        :label.font config/icon-font
-                                                        :label.padding_left 4
-                                                        :script (str config/plugin-dir "/space.clj")}))
-   (sketchybar/subscribe (space-key space) "mouse.clicked")
-   (sketchybar/subscribe (space-key space) :front_app_switched)))
-
 (def app-icons (edn/read-string (slurp (str (System/getProperty "user.home") "/.config/sketchybar/icon_map.edn"))))
 
 (defn get-icon [app] (let [icon (get app-icons app)] (if (nil? icon) ":default:" icon)))
@@ -46,14 +35,16 @@
 (defn build-icon-strip [apps]
   (if (or (nil? apps) (empty? apps) (every? empty? apps)) "—" (string/join " " (map get-icon apps))))
 
-(defn update-icons [space-key icons]
-  (sketchybar/exec (sketchybar/set space-key {:label icons})))
+(defn update-space [space-key icons highlight?]
+  (sketchybar/exec (sketchybar/set space-key {:label icons
+                                              :icon.highlight highlight?})))
 
 (defn refresh []
+  (when (= (System/getenv "SENDER") "mouse.clicked")
+    (shell {:continue true} "aerospace workspace" (string/replace (System/getenv "NAME") "space." "")))
   (doseq [monitor (get-monitors)]
-    (doseq [workspace (get-workspaces monitor)]
-      (println (str "setting up " workspace "/" monitor))
-      (add-space workspace monitor)
-      (update-icons (space-key workspace) (build-icon-strip (get-apps workspace))))))
+    (let [focused-workspace (get-focused-workspace monitor)]
+      (doseq [workspace (get-workspaces monitor)]
+        (update-space (space-key workspace) (build-icon-strip (get-apps workspace)) (= focused-workspace workspace))))))
 
 (refresh)
