368ea98c79899ba19666229f2ded3cc757779290
David Ferneding <fernedingd@gmail.com>
2024-09-20 22:58:16 +0200

    aerospace: dynamically load keymap
    Keybindings are always based on qwerty position and
    need to be remapped to work properly with different layouts

--- aerospace/setup.clj
+++ aerospace/setup.clj
@@ -1,13 +1,23 @@
 #!/usr/bin/env bb
-(require '[babashka.process :refer [shell]]
+(require '[babashka.process :refer [shell sh]]
          '[clojure.string :as string])
 
 (def config-folder (str (System/getProperty "user.home") "/.config/aerospace/"))
+
+(def keyboard-layout (string/replace
+                      (string/replace
+                       (:out (sh (str "defaults read "  (System/getProperty "user.home") "/Library/Preferences/com.apple.HIToolbox.plist" " AppleSelectedInputSources")))
+                       "\n" "")
+                      #".*\"KeyboardLayout Name\" = \"?(.*?)\"? ?;.*" "$1"))
 
 (def monitors (string/split (:out (shell {:out :string :err :string :continue true} "aerospace list-monitors")) #"\n"))
 
 (def uses-built-in? (true? (some #(string/includes? % "built-in") (map string/lower-case monitors))))
 (def external-monitor-count (if uses-built-in? (- (count monitors) 1) (count monitors)))
+
+(def keymap (case keyboard-layout
+              "Dvorak" (slurp (str config-folder "keymap.dvorak.toml"))
+              (slurp (str config-folder "keymap.qwerty.toml"))))
 
 (def base (slurp (str config-folder "base.toml")))
 
@@ -24,6 +34,6 @@
                        (str (if (> external-monitor-count 1) (slurp (str config-folder "workspaces.closed.left.toml")) "")
                             (if (> external-monitor-count 0) (slurp (str config-folder "workspaces.closed.right.toml")) "")))))
 
-(spit (str config-folder "aerospace.toml") (str base movement workspaces))
+(spit (str config-folder "aerospace.toml") (str keymap base movement workspaces))
 
 (shell {:continue true} "aerospace reload-config")
