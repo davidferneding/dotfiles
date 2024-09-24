789441161ed1a5b2047d5efa6037a9b3784babba
David Ferneding <fernedingd@gmail.com>
2024-09-24 16:28:40 +0200

    install: update script after test

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,10 @@
 # Tools/Shell/Misc
 brew install tmux
+brew install java
 brew install borkdude/brew/babashka
+brew install gum
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Apps
 brew install --cask firefox@developer-edition
@@ -10,10 +14,17 @@ brew install --cask wezterm
 brew install neovim
 
 brew install --cask nikitabobko/tap/aerospace
-cp ~/aerospace/aerospace.toml.default ~/aerospace/aerospace.toml
 
 brew tap FelixKratz/formulae
 brew install sketchybar
 brew install borders
 
 brew install lf
+
+gum style 'All dependencies have been installed' 'Close this terminal and open wezterm now' \
+    --border rounded \
+    --align center \
+    --margin "1 1" \
+    --padding "0 2" \
+    --foreground "#c6d0f5" \
+    --border-foreground "#eebebe"
