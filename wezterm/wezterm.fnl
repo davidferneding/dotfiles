6d0f76c5fcea20a9356516d4c4e65944e137b872
David Ferneding <fernedingd@gmail.com>
2024-10-22 16:02:10 +0200

    wezterm: refactor config

--- wezterm/wezterm.fnl
+++ wezterm/wezterm.fnl
@@ -1,20 +1,26 @@
+(fn merge-tables [tables]
+  (let [merged {}]
+    (each [_ tbl (ipairs tables)]
+      (each [k v (pairs tbl)]
+        (tset merged k v)))
+    merged))
+
 (local wezterm (require :wezterm))
 
 (local config (wezterm.config_builder))
 
-(local config-values
-       {;; appearance
-        :color_scheme "Catppuccin Macchiato"
+(local config-style
+       {:color_scheme "Catppuccin Macchiato"
         :font (wezterm.font "VictorMono Nerd Font" {:weight :Regular})
         :font_size 15
         :enable_tab_bar false
-        :window_padding {:left 0 :right 0 :top 0 :bottom 0}
-        :send_composed_key_when_left_alt_is_pressed true})
+        :window_padding {:left 0 :right 0 :top 0 :bottom 0}})
 
-;; test
+(local config-functional {:send_composed_key_when_left_alt_is_pressed true})
+
+(local config-values (merge-tables [config-style config-functional]))
+
 (each [key value (pairs config-values)]
   (tset config key value))
 
-(fn get [val] val)
-
-(get config)
+config
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,19 @@
 -- [nfnl] Compiled from wezterm.fnl by https://github.com/Olical/nfnl, do not edit.
+local function merge_tables(tables)
+  local merged = {}
+  for _, tbl in ipairs(tables) do
+    for k, v in pairs(tbl) do
+      merged[k] = v
+    end
+  end
+  return merged
+end
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local config_values = {color_scheme = "Catppuccin Macchiato", font = wezterm.font("VictorMono Nerd Font", {weight = "Regular"}), font_size = 15, window_padding = {left = 0, right = 0, top = 0, bottom = 0}, send_composed_key_when_left_alt_is_pressed = true, enable_tab_bar = false}
+local config_style = {color_scheme = "Catppuccin Macchiato", font = wezterm.font("VictorMono Nerd Font", {weight = "Regular"}), font_size = 15, window_padding = {left = 0, right = 0, top = 0, bottom = 0}, enable_tab_bar = false}
+local config_functional = {send_composed_key_when_left_alt_is_pressed = true}
+local config_values = merge_tables({config_style, config_functional})
 for key, value in pairs(config_values) do
   config[key] = value
 end
-local function get(val)
-  return val
-end
-return get(config)
+return config
