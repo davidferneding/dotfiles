1da14c3054b82ede278cc2b1686a6e1df83d5ecf
David Ferneding <fernedingd@gmail.com>
2024-11-06 13:38:58 +0100

    nvim: move treesitter config to fnl

--- nvim/lua/plugins/ext/treesitter.lua
+++ nvim/lua/plugins/ext/treesitter.lua
@@ -1,71 +1,7 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    version = "*",
-    build = ":TSUpdate",
-    opts = {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "clojure", "javascript", "typescript", "rust", "fennel", "c_sharp", 'yaml', 'json', 'go', 'angular', 'bash', 'dockerfile', 'gitcommit', 'helm' },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        -- List of parsers to ignore installing (or "all")
-        ignore_install = {},
-
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-        highlight = {
-            enable = true,
-
-            -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<tab>",
-                node_incremental = "<tab>",
-                node_decremental = "<BS>"
-            }
-        },
-        refactor = {
-            highlight_definitions = {
-                enable = true,
-                clear_on_cursor_move = true,
-            },
-            --     smart_rename = {
-            --       enable = true,
-            --       keymaps = {
-            --         smart_rename = "<leader>rr",
-            --       },
-            --     },
-            navigation = {
-                enable = true,
-                keymaps = {
-                    --         goto_definition = "<leader>jd",
-                    --         list_definitions = "<leader>ld",
-                    --         list_definitions_toc = "<leader>lc",
-                    goto_next_usage = "<leader>ju",
-                    goto_previous_usage = "<leader>ku",
-                },
-            },
-        },
-    }
-}
+-- [nfnl] Compiled from fnl/plugins/ext/treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local configs = require("nvim-treesitter.configs")
+  local config = {ensure_installed = "all", auto_install = true, ignore_install = {}, highlight = {enable = true, additional_vim_regex_highlighting = false}, incremental_selection = {enable = true, keymaps = {init_selection = "<tab>", node_incremental = "<tab>", node_decremental = "<BS>"}}, refactor = {highlight_definitions = {enable = true, clear_on_cursor_move = true}, smart_rename = {enable = true, keymaps = {smart_rename = "<leader>rs"}}, navigation = {enable = true, keymaps = {list_definitions_toc = "<leader>lc", goto_next_usage = "<leader>ju", goto_previous_usage = "<leader>ku"}}}, sync_install = false}
+  return configs.setup(config)
+end
+return {"nvim-treesitter/nvim-treesitter", version = "*", build = ":TSUpdate", config = _1_}
