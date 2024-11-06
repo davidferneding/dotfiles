8c42c26b7f2d893d7447536c08250726c1039cf0
David Ferneding <fernedingd@gmail.com>
2024-11-06 13:39:35 +0100

    nvim: move vim-jack-in config to fnl

--- nvim/lua/plugins/ext/vim-jack-in.lua
+++ nvim/lua/plugins/ext/vim-jack-in.lua
@@ -1,3 +1,2 @@
-return {
-    "clojure-vim/vim-jack-in"
-}
+-- [nfnl] Compiled from fnl/plugins/ext/vim-jack-in.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"clojure-vim/vim-jack-in"}
