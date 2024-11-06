480987e345c8c4669bfafc79954937b4514d4732
David Ferneding <fernedingd@gmail.com>
2024-11-06 13:38:12 +0100

    nvim: move telescope config to fnl

--- nvim/lua/plugins/ext/telescope.lua
+++ nvim/lua/plugins/ext/telescope.lua
@@ -1,19 +1,9 @@
-return {
-    'nvim-telescope/telescope.nvim',
-    version = '*',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require("telescope").load_extension("noice")
-
-        local actions = require("telescope.actions")
-        require("telescope").setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<esc>"] = actions.close,
-                    },
-                },
-            },
-        })
-    end
-}
+-- [nfnl] Compiled from fnl/plugins/ext/telescope.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local telescope = require("telescope")
+  telescope.load_extension("noice")
+  local actions = require("telescope.actions")
+  local config = {defaults = {mappings = {i = {["<esc>"] = actions.close}}}}
+  return telescope.setup(config)
+end
+return {"nvim-telescope/telescope.nvim", version = "*", dependencies = {"nvim-lua/plenary.nvim"}, config = _1_}
