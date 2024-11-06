db1180a942cfcf5b560ab3c70074d10599bb4f89
David Ferneding <fernedingd@gmail.com>
2024-11-06 13:39:52 +0100

    nvim: move vsnip config to fnl

---
+++ nvim/fnl/plugins/ext/vsnip.fnl
@@ -0,0 +1,1 @@
+[{1 :hrsh7th/cmp-vsnip} {1 :hrsh7th/vim-vsnip} {1 :hrsh7th/vim-vsnip-integ}]
--- nvim/lua/plugins/ext/vsnip.lua
+++ nvim/lua/plugins/ext/vsnip.lua
@@ -1,5 +1,2 @@
-return {
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
-    'hrsh7th/vim-vsnip-integ'
-}
+-- [nfnl] Compiled from fnl/plugins/ext/vsnip.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"hrsh7th/cmp-vsnip"}, {"hrsh7th/vim-vsnip"}, {"hrsh7th/vim-vsnip-integ"}}
