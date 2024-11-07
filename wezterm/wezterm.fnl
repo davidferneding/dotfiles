61d12a412b4f3b599d5658214c424ebe68a72519
David Ferneding <fernedingd@gmail.com>
2024-11-07 12:07:41 +0100

    wezterm: use visual instead of audible bell

--- wezterm/wezterm.fnl
+++ wezterm/wezterm.fnl
@@ -7,19 +7,25 @@
 
 (local wezterm (require :wezterm))
 
-(local config (wezterm.config_builder))
-
 (local config-style
        {:color_scheme "Catppuccin Frappe"
+        :colors {:visual_bell "#e78284"}
         :font (wezterm.font "VictorMono Nerd Font" {:weight :Regular})
         :font_size 15
         :enable_tab_bar false
         :window_padding {:left 0 :right 0 :top 0 :bottom 0}})
 
+(local config-feel {:audible_bell :Disabled
+                    :visual_bell {:fade_in_duration_ms 75
+                                  :fade_out_duration_ms 75
+                                  :target :CursorColor}})
+
 (local config-functional {:send_composed_key_when_left_alt_is_pressed true})
 
-(local config-values (merge-tables [config-style config-functional]))
+(local config-values
+       (merge-tables [config-style config-feel config-functional]))
 
+(local config (wezterm.config_builder))
 (each [key value (pairs config-values)]
   (tset config key value))
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,10 +9,11 @@ local function merge_tables(tables)
   return merged
 end
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-local config_style = {color_scheme = "Catppuccin Frappe", font = wezterm.font("VictorMono Nerd Font", {weight = "Regular"}), font_size = 15, window_padding = {left = 0, right = 0, top = 0, bottom = 0}, enable_tab_bar = false}
+local config_style = {color_scheme = "Catppuccin Frappe", colors = {visual_bell = "#e78284"}, font = wezterm.font("VictorMono Nerd Font", {weight = "Regular"}), font_size = 15, window_padding = {left = 0, right = 0, top = 0, bottom = 0}, enable_tab_bar = false}
+local config_feel = {audible_bell = "Disabled", visual_bell = {fade_in_duration_ms = 75, fade_out_duration_ms = 75, target = "CursorColor"}}
 local config_functional = {send_composed_key_when_left_alt_is_pressed = true}
-local config_values = merge_tables({config_style, config_functional})
+local config_values = merge_tables({config_style, config_feel, config_functional})
+local config = wezterm.config_builder()
 for key, value in pairs(config_values) do
   config[key] = value
 end
