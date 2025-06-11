1a0d963af7b322ba6acb79b740baa8d981c1e0fd
David Ferneding <fernedingd@gmail.com>
2025-06-11 21:31:04 +0200

    aerospace: update workspace to monitor config

--- aerospace/setup.clj
+++ aerospace/setup.clj
@@ -12,9 +12,6 @@
 
 (def monitors (string/split (:out (shell {:out :string :err :string :continue true} "aerospace list-monitors")) #"\n"))
 
-(def uses-built-in? (true? (some #(string/includes? % "built-in") (map string/lower-case monitors))))
-(def external-monitor-count (if uses-built-in? (- (count monitors) 1) (count monitors)))
-
 (def keymap (case keyboard-layout
               "Dvorak" (slurp (str config-folder "keymap.dvorak.toml"))
               (slurp (str config-folder "keymap.qwerty.toml"))))
@@ -22,17 +19,14 @@
 (def base (slurp (str config-folder "base.toml")))
 
 (def movement (str (slurp (str config-folder "movement.base.toml"))
-                   (if uses-built-in? (slurp (str config-folder "movement.built-in.toml")) "")
-                   (if (> external-monitor-count 1) (slurp (str config-folder "movement.left.toml")) "")
-                   (if (> external-monitor-count 0) (slurp (str config-folder "movement.right.toml")) "")))
+                   (if (= (count monitors) 1) (slurp (str config-folder "movement.one.toml")) "")
+                   (if (= (count monitors) 2) (slurp (str config-folder "movement.two.toml")) "")
+                   (if (= (count monitors) 3) (slurp (str config-folder "movement.three.toml")) "")))
 
 (def workspaces (str (slurp (str config-folder "workspaces.base.toml"))
-                     (if uses-built-in?
-                       (str (slurp (str config-folder "workspaces.built-in.toml"))
-                            (if (> external-monitor-count 1) (slurp (str config-folder "workspaces.open.left.toml")) "")
-                            (if (> external-monitor-count 0) (slurp (str config-folder "workspaces.open.right.toml")) ""))
-                       (str (if (> external-monitor-count 1) (slurp (str config-folder "workspaces.closed.left.toml")) "")
-                            (if (> external-monitor-count 0) (slurp (str config-folder "workspaces.closed.right.toml")) "")))))
+                     (if (= (count monitors) 1) (slurp (str config-folder "workspaces.one.toml")) "")
+                     (if (= (count monitors) 2) (slurp (str config-folder "workspaces.two.toml")) "")
+                     (if (= (count monitors) 3) (slurp (str config-folder "workspaces.three.toml")) "")))
 
 (spit (str config-folder "aerospace.toml") (str keymap base movement workspaces))
 
