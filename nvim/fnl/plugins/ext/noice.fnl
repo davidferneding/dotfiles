0fea3f856f23a28661256e2aeaa2271db14f605e
David Ferneding <fernedingd@gmail.com>
2025-06-25 18:12:31 +0200

    nvim: fix noice cmdline suggestions

--- nvim/fnl/plugins/ext/noice.fnl
+++ nvim/fnl/plugins/ext/noice.fnl
@@ -22,7 +22,7 @@
                          ;; you can enable a preset for easier configuration
                          :presets {:bottom_search true
                                    ;; use a classic bottom cmdline for search
-                                   :command_palette true
+                                   :command_palette false
                                    ;; position the cmdline and popupmenu together
                                    :long_message_to_split true
                                    ;; long messages will be sent to a split
--- nvim/lua/plugins/ext/noice.lua
+++ nvim/lua/plugins/ext/noice.lua
@@ -1,8 +1,8 @@
--- [nfnl] Compiled from fnl/plugins/ext/noice.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] fnl/plugins/ext/noice.fnl
 local function _1_()
   local notify = require("notify")
   notify.setup({background_colour = "#6e738d"})
   local noice = require("noice")
-  return noice.setup({cmdline = {view = "cmdline"}, lsp = {override = {[{"vim.lsp.util.convert_input_to_markdown_lines"}] = true, [{"vim.lsp.util.stylize_markdown"}] = true, [{"cmp.entry.get_documentation"}] = true}}, presets = {bottom_search = true, command_palette = true, long_message_to_split = true, lsp_doc_border = true, inc_rename = false}})
+  return noice.setup({cmdline = {view = "cmdline"}, lsp = {override = {[{"vim.lsp.util.convert_input_to_markdown_lines"}] = true, [{"vim.lsp.util.stylize_markdown"}] = true, [{"cmp.entry.get_documentation"}] = true}}, presets = {bottom_search = true, long_message_to_split = true, lsp_doc_border = true, command_palette = false, inc_rename = false}})
 end
 return {"folke/noice.nvim", version = "*", event = "VeryLazy", dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}, config = _1_}
