4e3d5afb83cabb650cedd4635977e684c7040f47
David Ferneding <fernedingd@gmail.com>
2025-10-01 20:27:21 +0200

    nvim: add keybind to search keybinds

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -6,5 +6,6 @@ return {
         { "<leader><leader>", "<cmd>FzfLua files<cr>",        desc = "Search files using fzf" },
         { "<leader>/",        "<cmd>FzfLua live_grep_native<cr>", desc = "Live grep project using fzf" },
         { "<leader>/",        "<cmd>FzfLua grep_visual<cr>",      mode = "v",                          desc = "Grep selection using fzf" },
+        { "<leader>?",        "<cmd>FzfLua keymaps<cr>", desc = "Search through keymap" },
     },
 }
