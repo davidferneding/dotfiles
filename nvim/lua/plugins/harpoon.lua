2f28d03ce69edabbaac91a1442826381d6e9f46d
David Ferneding <fernedingd@gmail.com>
2025-10-01 20:27:39 +0200

    nvim: add cycling to harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,9 +4,10 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     keys = {
-        { "<leader>a", function() harpoon:list():add() end,                         desc = "Add to harpoon list" },
-        { "<C-h>",     function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle harpoon list" },
-        { "<C-S-h>",   function() harpoon:list():prev() end,                        desc = "Go to previous harpoon buffer" },
-        { "<C-S-s>",   function() harpoon:list():next() end,                        desc = "Go to next harpoon buffer" },
+        { "<leader>a", function() require("harpoon"):list():add() end,                                    desc = "Add to harpoon list" },
+        { "<leader>r", function() require("harpoon"):list():remove() end,                                 desc = "Remove from harpoon list" },
+        { "<C-h>",     function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Toggle harpoon list" },
+        { "<leader>h", function() require("harpoon"):list():prev({ ui_nav_wrap = true }) end,             desc = "Go to previous harpoon buffer" },
+        { "<leader>s", function() require("harpoon"):list():next({ ui_nav_wrap = true }) end,             desc = "Go to next harpoon buffer" },
     },
 }
